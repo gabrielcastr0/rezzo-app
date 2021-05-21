838d9cde7fd13af4175f17d9470b1074f04cccfb
Gabriel Castro <gabrielcastro.xy@gmail.com>
2021-05-21 15:30:21 -0300

    Iniciando app

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>Working...</Text>
+    </View>
+  )
+}
+
+export default App;
