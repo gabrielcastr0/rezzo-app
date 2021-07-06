f2cf8822be32ac21814d035df52b6a7c8ffd8c7e
Gabriel Castro <gabrielcastro.xy@gmail.com>
2021-07-06 16:06:13 -0300

    feat: Modal na tela home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useRef, useEffect} from 'react';
 import S from './styles';
 import {useTheme} from '../../contexts/ThemeProvider';
 import CustomBackgroundImage from '../../components/CustomBackgroundImage';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import {Modalize} from 'react-native-modalize';
 import {LocaleConfig} from 'react-native-calendars';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 LocaleConfig.locales.pt_br = {
   monthNames: [
@@ -52,16 +53,25 @@ LocaleConfig.defaultLocale = 'pt_br';
 const Home = () => {
   const {theme} = useTheme();
 
+  useEffect(() => {
+    modalizeRef.current?.open();
+  }, []);
+
   const DateActual = new Date();
   console.log(DateActual);
+
+  const modalizeRef = useRef(null);
+
+  const onOpen = day => {
+    console.log(day.day);
+    modalizeRef.current?.open();
+  };
 
   return (
     <S.Container style={{backgroundColor: theme.backgroundColor}}>
       <Calendar
         enableSwipeMonths={true}
-        onDayPress={day => {
-          alert(`O dia selecionado é: ${day.day}`);
-        }}
+        onDayPress={onOpen}
         // Specify style for calendar container element. Default = {}
         style={{}}
         // Specify theme properties to override specific styles for calendar parts. Default = {}
@@ -94,8 +104,36 @@ const Home = () => {
         }}
       />
       <CustomBackgroundImage />
+
+      <Modalize
+        ref={modalizeRef}
+        snapPoint={300}
+        modalHeight={500}
+        HeaderComponent={
+          <S.ModalizeHeaderView>
+            <S.ModalizeHeaderText style={{color: theme.textColor}}>
+              day + month
+            </S.ModalizeHeaderText>
+          </S.ModalizeHeaderView>
+        }>
+        <S.ModalizeView>
+          {/* <TouchableOpacity
+            style={[styles.botao, {backgroundColor: '#29ae19'}]}>
+            <Text>EDITAR</Text>
+          </TouchableOpacity> */}
+        </S.ModalizeView>
+      </Modalize>
     </S.Container>
   );
 };
+
+const styles = StyleSheet.create({
+  botao: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    borderRadius: 7,
+  },
+});
 
 export default Home;
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -10,4 +10,22 @@ export default {
     font-weight: bold;
     position: absolute;
   `,
+
+  ModalizeHeaderView: styled.View`
+    height: 50px;
+    justify-content: center;
+    padding-left: 15px;
+  `,
+
+  ModalizeHeaderText: styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+  `,
+
+  ModalizeView: styled.View`
+    flex: 1;
+    height: 180px;
+    flex-direction: row;
+    align-items: center;
+  `,
 };
